bcae8f1d89fb3b13278d92316bf636d541b5355f
Ilia <111631328+Citxr@users.noreply.github.com>
2025-05-19 18:11:32 +0300

    Update server.js

--- catalog-server/server.js
+++ catalog-server/server.js
@@ -2,10 +2,14 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const PRODUCTS_FILE = path.join(__dirname, 'products.json');
 
+const instanceId = Math.floor(Math.random() * 1000);
+
 const server = http.createServer((req, res) => {
+    console.log(`[Instance ${instanceId}] Request: ${req.method} ${req.url}`);
+
     if (req.url === '/' && req.method === 'GET') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
             if (err) {
@@ -22,8 +26,16 @@ const server = http.createServer((req, res) => {
                 res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Internal Server Error');
             } else {
+                const products = JSON.parse(data);
+                const response = {
+                    products: products,
+                    serverInfo: {
+                        instanceId: instanceId,
+                        port: PORT
+                    }
+                };
                 res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(data);
+                res.end(JSON.stringify(response));
             }
         });
     } else {
@@ -33,5 +45,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-    console.log(`Catalog server is running on http://localhost:${PORT}`);
+    console.log(`Catalog server instance ${instanceId} is running on http://localhost:${PORT}`);
 });
